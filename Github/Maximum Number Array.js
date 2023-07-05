268399762bf7cee48a4d513fb18f586d3f7969f6
African-beep <80663571+African-beep@users.noreply.github.com>
2023-07-05 23:16:56 +0300

    Update Maximum Number Array.js

--- Github/Maximum Number Array.js
+++ Github/Maximum Number Array.js
@@ -1,3 +1,5 @@
+//To find the maximum number in an array using the provided function. This code will iterate over each number in the nums array and update max_num whenever a larger number is found. Finally, it will return the maximum number found in the array.//
+
 function find_max(nums) {
     let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
     for (let num of nums) {
